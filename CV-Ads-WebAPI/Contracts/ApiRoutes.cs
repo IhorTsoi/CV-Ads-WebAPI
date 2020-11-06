3a8e4cf9359d71a423cea7b00b93e674e0788cea
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-06 14:24:45 +0200

    Static class ApiRoutes was created for further routes storing.

---
+++ CV-Ads-WebAPI/Contracts/ApiRoutes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CV_Ads_WebAPI.Contracts
+{
+    public static class ApiRoutes
+    {
+    }
+}
