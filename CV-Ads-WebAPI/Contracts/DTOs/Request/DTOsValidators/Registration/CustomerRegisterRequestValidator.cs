0692ecbdabadfe74d87480c4445127df2a67537d
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-09 14:58:45 +0200

    Registration validators were updated with localization.

--- CV-Ads-WebAPI/Contracts/DTOs/Request/DTOsValidators/Registration/CustomerRegisterRequestValidator.cs
+++ CV-Ads-WebAPI/Contracts/DTOs/Request/DTOsValidators/Registration/CustomerRegisterRequestValidator.cs
@@ -1,18 +1,24 @@
 ﻿using CV_Ads_WebAPI.Contracts.DTOs.Request.Registration;
 using FluentValidation;
 using FluentValidation.Validators;
+using Microsoft.Extensions.Localization;
 
 namespace CV_Ads_WebAPI.Contracts.DTOs.Request.DTOsValidators.Registration
 {
     public class CustomerRegisterRequestValidator : AbstractValidator<CustomerRegisterRequest>
     {
-        public CustomerRegisterRequestValidator()
+        public CustomerRegisterRequestValidator(IStringLocalizer localizer)
         {
-            RuleFor(request => request.Email).NotEmpty().EmailAddress(EmailValidationMode.AspNetCoreCompatible)
-                .WithMessage("The email is incorrect.");
-            RuleFor(request => request.Password).NotEmpty().WithMessage("The password field must not be empty.");
-            RuleFor(request => request.FirstName).NotEmpty().WithMessage("The first name field must not be empty.");
-            RuleFor(request => request.LastName).NotEmpty().WithMessage("The last name field must not be empty.");
+            RuleFor(request => request.Email).NotEmpty()
+                .WithMessage(localizer["The email is incorrect."])
+                .EmailAddress(EmailValidationMode.AspNetCoreCompatible)
+                .WithMessage(localizer["The email is incorrect."]);
+            RuleFor(request => request.Password).NotEmpty()
+                .WithMessage(localizer["The password field must not be empty."]);
+            RuleFor(request => request.FirstName).NotEmpty()
+                .WithMessage(localizer["The first name field must not be empty."]);
+            RuleFor(request => request.LastName).NotEmpty()
+                .WithMessage(localizer["The last name field must not be empty."]);
         }
     }
 }
