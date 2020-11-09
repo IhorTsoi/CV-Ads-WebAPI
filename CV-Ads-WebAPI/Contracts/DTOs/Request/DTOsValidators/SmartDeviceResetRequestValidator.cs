084ea57fff329e21aa5722e03cc94281036f8f29
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-09 15:12:36 +0200

    Unnecessary usings were removed.

--- CV-Ads-WebAPI/Contracts/DTOs/Request/DTOsValidators/SmartDeviceResetRequestValidator.cs
+++ CV-Ads-WebAPI/Contracts/DTOs/Request/DTOsValidators/SmartDeviceResetRequestValidator.cs
@@ -1,8 +1,4 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CV_Ads_WebAPI.Contracts.DTOs.Request.DTOsValidators
 {
