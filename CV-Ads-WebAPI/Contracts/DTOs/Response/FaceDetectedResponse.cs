343fe9f932064d312aace6a590294c54b7d7e0e9
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-09 19:33:38 +0200

    Response DTO for face detection endpoint was created.

---
+++ CV-Ads-WebAPI/Contracts/DTOs/Response/FaceDetectedResponse.cs
@@ -0,0 +1,16 @@
+﻿using CV_Ads_WebAPI.Domain.Models;
+
+namespace CV_Ads_WebAPI.Contracts.DTOs.Response
+{
+    public class FaceDetectedResponse
+    {
+        public FaceDetectedResponse(Gender gender, int? age)
+        {
+            Gender = gender;
+            Age = age;
+        }
+
+        public Gender Gender { get; set; }
+        public int? Age { get; set; }
+    }
+}
