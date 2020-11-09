15749aa4cc36480b77073b1383885e45627b2cfb
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-09 14:56:56 +0200

    Unnecessary using were removed.

--- CV-Ads-WebAPI/ServiceInstallation/Installers/ServicesInstaller.cs
+++ CV-Ads-WebAPI/ServiceInstallation/Installers/ServicesInstaller.cs
@@ -2,10 +2,6 @@
 using CV_Ads_WebAPI.Services.UserServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CV_Ads_WebAPI.ServiceInstallation.Installers
 {
