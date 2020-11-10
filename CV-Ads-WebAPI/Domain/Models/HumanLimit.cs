4376079c8a786828d3305e6a9160b90dea6d0191
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-10 09:38:45 +0200

    HumanLimit's properties 'MinAge' and 'MaxAge' were made not nullable.

--- CV-Ads-WebAPI/Domain/Models/HumanLimit.cs
+++ CV-Ads-WebAPI/Domain/Models/HumanLimit.cs
@@ -4,10 +4,13 @@ namespace CV_Ads_WebAPI.Domain.Models
 {
     public class HumanLimit
     {
+        public const int MIN_AGE = 0;
+        public const int MAX_AGE = 100;
+
         private HumanLimit()
         { }
 
-        public HumanLimit(Gender gender, int? minAge, int? maxAge)
+        public HumanLimit(Gender gender, int minAge, int maxAge)
         {
             Id = Guid.NewGuid();
             Gender = gender;
@@ -17,8 +20,8 @@ namespace CV_Ads_WebAPI.Domain.Models
 
         public Guid Id { get; set; }
         public Gender Gender { get; set; }
-        public int? MinAge { get; set; }
-        public int? MaxAge { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
 
         public Advertisement Advertisement { get; set; }
     }
---
+++ CV-Ads-WebAPI/Migrations/20201110073538_HumanLimit_min_max_ages_are_mandatory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CV_Ads_WebAPI.Migrations
+{
+    public partial class HumanLimit_min_max_ages_are_mandatory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinAge",
+                table: "HumanLimit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxAge",
+                table: "HumanLimit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinAge",
+                table: "HumanLimit",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxAge",
+                table: "HumanLimit",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
