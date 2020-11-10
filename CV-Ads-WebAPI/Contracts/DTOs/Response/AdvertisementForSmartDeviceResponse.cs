a54c3c2b054171e475cbcbad3793226803e8b918
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-10 23:06:09 +0200

    Create AdvertisementForSmartDeviceResponse.cs

---
+++ CV-Ads-WebAPI/Contracts/DTOs/Response/AdvertisementForSmartDeviceResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace CV_Ads_WebAPI.Contracts.DTOs.Response
+{
+    public class AdvertisementForSmartDeviceResponse
+    {
+        public AdvertisementForSmartDeviceResponse(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
