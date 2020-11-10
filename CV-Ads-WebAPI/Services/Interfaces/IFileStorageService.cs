04921b1f40a86a137cee1c0a4cd3dcaf042fb6ba
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-10 16:44:53 +0200

    IFileStorage interface was added.

---
+++ CV-Ads-WebAPI/Services/Interfaces/IFileStorageService.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace CV_Ads_WebAPI.Services.Interfaces
+{
+    public interface IFileStorageService
+    {
+        public Task SaveFileAsync(string filename, Stream uploadStream);
+        public string GetUrlForFile(string filename);
+    }
+}
