a08e8beeed9fa7df87d287d6b9ad4a749c06f988
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-10 16:46:40 +0200

    Request->Domain AutoMapper profile was added.

---
+++ CV-Ads-WebAPI/AutoMapper/Profiles/RequestToDomainProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CV_Ads_WebAPI.Contracts.DTOs.Request.AdvertisementCreation;
+using CV_Ads_WebAPI.Domain.Models;
+using System.IO;
+
+namespace CV_Ads_WebAPI.AutoMapper.Profiles
+{
+    public class RequestToDomainProfile : Profile
+    {
+        public RequestToDomainProfile()
+        {
+            CreateMap<TimePeriodLimitRequest, TimePeriodLimit>();
+            CreateMap<HumanLimitRequest, HumanLimit>();
+
+            CreateMap<CreateAdvertisementRequest, Advertisement>()
+                .AfterMap((dto, ad) => ad.PictureExtension = Path.GetExtension(dto.FormFile.FileName));
+        }
+    }
+}
