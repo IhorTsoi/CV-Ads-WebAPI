819e224ea0e497993425e2c70261d503f83a316f
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-11 13:24:24 +0200

    ImportServiceDataRequest DTO was created.

---
+++ CV-Ads-WebAPI/Contracts/DTOs/Request/ImportServiceDataRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CV_Ads_WebAPI.Contracts.DTOs.Request
+{
+    public class ImportServiceDataRequest
+    {
+        public IFormFile FormFile { get; set; }
+    }
+}
