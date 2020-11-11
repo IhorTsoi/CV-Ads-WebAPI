9a41dde4b4ee2a6be5157c955d9b2c0f71ec47a2
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-11 20:00:21 +0200

    ActivateSmartDeviceRequest DTO.

---
+++ CV-Ads-WebAPI/Contracts/DTOs/DTORequestValidators/ActivateSmartDeviceRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using CV_Ads_WebAPI.Contracts.DTOs.Request;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace CV_Ads_WebAPI.Contracts.DTOs.DTORequestValidators
+{
+    public class ActivateSmartDeviceRequestValidator : AbstractValidator<ActivateSmartDeviceRequest>
+    {
+        public ActivateSmartDeviceRequestValidator(IStringLocalizer localizer)
+        {
+            RuleFor(request => request.SerialNumber)
+                .NotEmpty()
+                .WithMessage(localizer["The serial number field must not be empty."]);
+
+            RuleFor(request => request.DefaultPassword)
+                .NotEmpty()
+                .WithMessage(localizer["The password field must not be empty."]);
+
+            RuleFor(request => request.NewPassword)
+                .NotEmpty()
+                .WithMessage(localizer["The new password field must not be empty."]);
+        }
+    }
+}
---
+++ CV-Ads-WebAPI/Contracts/DTOs/Request/ActivateSmartDeviceRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace CV_Ads_WebAPI.Contracts.DTOs.Request
+{
+    public class ActivateSmartDeviceRequest
+    {
+        public string SerialNumber { get; set; }
+        public string DefaultPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
