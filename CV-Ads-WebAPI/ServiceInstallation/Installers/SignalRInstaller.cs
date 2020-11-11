0a19b62716fb2e2f1f2c9bd515b40af028e1bc0a
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-11-11 18:05:21 +0200

    Create SignalRInstaller.cs

---
+++ CV-Ads-WebAPI/ServiceInstallation/Installers/SignalRInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace CV_Ads_WebAPI.ServiceInstallation.Installers
+{
+    public class SignalRInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSignalR();
+        }
+    }
+}
