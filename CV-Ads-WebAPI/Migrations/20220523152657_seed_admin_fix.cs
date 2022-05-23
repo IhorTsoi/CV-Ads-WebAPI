6fdffc6416c952d998429bec50856ba342761d49
Ihor Tsoi <ihor.tsoi@nure.ua>
2022-05-23 18:31:08 +0300

    Update migrations.

---
+++ CV-Ads-WebAPI/Migrations/20220523152657_seed_admin_fix.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CV_Ads_WebAPI.Migrations
+{
+    public partial class seed_admin_fix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Admins",
+                keyColumn: "Id",
+                keyValue: new Guid("1ec7309f-c97d-412c-b8b8-31c1459cbd41"),
+                columns: new[] { "FirstName", "LastName" },
+                values: new object[] { "Default", "Admin" });
+
+            migrationBuilder.UpdateData(
+                table: "UserIdentities",
+                keyColumn: "Id",
+                keyValue: new Guid("1ec7309f-c97d-412c-b8b8-31c1459cbd41"),
+                column: "Login",
+                value: "adam@nure.ua");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Admins",
+                keyColumn: "Id",
+                keyValue: new Guid("1ec7309f-c97d-412c-b8b8-31c1459cbd41"),
+                columns: new[] { "FirstName", "LastName" },
+                values: new object[] { "Ihor", "Tsoi" });
+
+            migrationBuilder.UpdateData(
+                table: "UserIdentities",
+                keyColumn: "Id",
+                keyValue: new Guid("1ec7309f-c97d-412c-b8b8-31c1459cbd41"),
+                column: "Login",
+                value: "qweqwe");
+        }
+    }
+}
